3839e4e934ef390dd1fbffde2a5cff1d4e7d67d6
svgonline@ya.ru <svgonline@ya.ru>
2022-02-25 15:11:14 +0500

    s

---
+++ public/loginPage.js
@@ -0,0 +1,19 @@
+'use strict'
+const userForm = new UserForm();
+userForm.loginFormCallback = data => ApiConnector.login(data, response => {
+    if (!response.success) {
+        return userForm.setLoginErrorMessage(response.error);
+    }
+    userForm.loginFormAction(response);
+    location.reload();
+    return;
+});
+
+userForm.registerFormCallback = newData => ApiConnector.register(newData, response => {
+    if (!response.success) {
+        return userForm.setRegisterErrorMessage(response.error);
+    }
+    userForm.registerFormAction(response);
+    location.reload();
+    return;
+});
